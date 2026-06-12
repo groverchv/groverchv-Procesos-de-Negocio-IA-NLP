29fa68027351d9313ea954a39bd0e706db24c432
Grover <groverchoquevillca80@gmail.com>
2026-06-12 14:46:45 -0400

    Ajuste de IU

--- Movil/lib/services/api_service.dart
+++ Movil/lib/services/api_service.dart
@@ -21,6 +21,9 @@ class ApiService {
 
   String get iaUrl {
     if (useLocalIA) {
+      if (defaultTargetPlatform == TargetPlatform.android && !kIsWeb) {
+        return 'http://10.0.2.2:8000';
+      }
       return 'http://localhost:8000';
     }
     if (_iaBaseEnv.isNotEmpty) {
