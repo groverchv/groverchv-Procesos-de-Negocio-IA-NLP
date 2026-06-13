b5807c865bb479bbec6092c5619922e53b2e9da5
Grover <groverchoquevillca80@gmail.com>
2026-06-12 20:10:10 -0400

    IA Local.

--- Movil/lib/services/api_service.dart
+++ Movil/lib/services/api_service.dart
@@ -21,9 +21,6 @@ class ApiService {
 
   String get iaUrl {
     if (useLocalIA) {
-      if (defaultTargetPlatform == TargetPlatform.android && !kIsWeb) {
-        return 'http://10.0.2.2:8000';
-      }
       return 'http://localhost:8000';
     }
     if (_iaBaseEnv.isNotEmpty) {
